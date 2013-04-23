657d315fdcba08486dbabc280e63a102aed1a5ed
Julien Bourgain <julien.bourgain@zenika.com>
2013-04-23 10:21:06 +0200

    Usercontroller

--- src/main/java/com/zenika/zenapp/controllers/UserController.java
+++ src/main/java/com/zenika/zenapp/controllers/UserController.java
@@ -1,11 +1,38 @@
 package com.zenika.zenapp.controllers;
 
+import com.zenika.zenapp.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created with IntelliJ IDEA.
- * User: zenika
- * Date: 22/04/13
- * Time: 16:25
- * To change this template use File | Settings | File Templates.
+ * @author Julien Bourgain
  */
-public interface UserController {
+@Controller
+public class UserController {
+//    @RequestMapping(value = "/user", method = RequestMethod.GET, produces = "application/json")
+//    public @ResponseBody List<User> list() {
+//        return new ArrayList<User>(userRepository.values());
+//    }
+//    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = "application/json")
+//    public @ResponseBody User getById(@PathVariable long id) {
+//        return userRepository.get(id);
+//    }
+//
+//    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void create(@RequestBody User user) {
+//        long id = userIdGenerator.incrementAndGet();
+//        user.setId(id);
+//        userRepository.put(id, user);
+//    }
+//
+//    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void delete(@PathVariable long id) {
+//        userRepository.remove(id);
+//    }
 }
